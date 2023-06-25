c750d37986067e85502bf0f535745b9c9d421eda
sarahwambui96 <sarahwambui843@gmail.com>
2023-06-26 02:23:53 +0300

    completed assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -8,15 +8,17 @@ class GetRequester:
 
     def get_response_body(self):
         response = requests.get(self.url)
-        return response.text
+        return response.content
+    
 
     def load_json(self):
         response_body = self.get_response_body()
-        return json.loads(response_body)
+        json_data = response_body.decode('utf-8')
+        return json.loads(json_data)
     
 
-get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
-data = get_requester.load_json()
+requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json').get_response_body()
+data = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json').load_json()
 print(data)
 
 # execute the file as a script
